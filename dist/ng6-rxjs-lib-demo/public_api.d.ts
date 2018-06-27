f1599cbded6bc1d6332031cb373c00ba6a95740c
Rolland <gisuser.earth@gmail.com>
2018-06-26 20:02:32 -0400

    dist publisth

---
+++ dist/ng6-rxjs-lib-demo/public_api.d.ts
@@ -0,0 +1,4 @@
+export { Ng6RxjsLibDemoService } from './lib/ng6-rxjs-lib-demo.service';
+export { Ng6RxjsLibDemoModule } from './lib/ng6-rxjs-lib-demo.module';
+export { Ng6RxjsLibDemoComponent } from './lib/ng6-rxjs-lib-demo.component';
+export { SimpleTableComponent } from './lib/components/simple-table/simple-table.component';
